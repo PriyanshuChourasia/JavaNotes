e7043b2af8f0cd2f725b2148a718836780e97d54
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2024-12-24 08:22:24 +0530

    Excercise to print a value done

---
+++ src/Excercise/Excercise_1.java
@@ -0,0 +1,9 @@
+package Excercise;
+
+public class Excercise_1 {
+    public static void main(String args[]){
+//        Two ways to print in new line
+        System.out.println("Hello");
+        System.out.println("Priyanshu \nChourasia");
+    }
+}
